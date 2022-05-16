9afaba81b7e06a18814ceaf297f5a054ad7dee7a
marcos echeverria <marcosalbertoecheverria@gmail.com>
2022-05-16 12:51:23 -0300

    header fix

--- index.js
+++ index.js
@@ -109,12 +109,3 @@ form.addEventListener("submit", (e) => {
 		document.getElementById();
 	}
 } );
-
-function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
-
-  document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle);
